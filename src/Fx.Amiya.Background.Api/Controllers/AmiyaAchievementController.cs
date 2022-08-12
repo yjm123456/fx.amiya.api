9e060f7887e05b036f084fcf56ea1f8ef142ab56
shangheweilai <yinwujun1319@163.com>
2022-08-12 16:49:22 +0800

    业绩板块新建控制层

---
+++ src/Fx.Amiya.Background.Api/Controllers/AmiyaAchievementController.cs
@@ -0,0 +1,110 @@
+﻿using Fx.Amiya.Background.Api.Vo.Appointment;
+using Fx.Amiya.Background.Api.Vo.OrderReport;
+using Fx.Amiya.Core.Interfaces.Goods;
+using Fx.Amiya.IDal;
+using Fx.Amiya.IService;
+using Fx.Amiya.Service;
+using Fx.Authorization.Attributes;
+using Fx.Common;
+using Fx.Open.Infrastructure.Web;
+using Jd.Api.Util;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fx.Amiya.Background.Api.Controllers
+{
+
+    /// <summary>
+    /// 啊美雅业绩接口
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class AmiyaAchievementController : ControllerBase
+    {
+        private IOrderService orderService;
+        private ISendOrderInfoService _sendOrderInfoService;
+        private IShoppingCartRegistrationService _shoppingCartRegistrationService;
+        private IContentPlatFormOrderDealInfoService _contentPlatFormOrderDealInfoService;
+        private IHospitalInfoService _hospitalInfoService;
+        private ICustomerService customerService;
+        private IHttpContextAccessor httpContextAccessor;
+        private IContentPlateFormOrderService _contentPlatFormOrderService;
+        private IContentPlatformOrderSendService _sendContentPlatFormOrderInfoService;
+        private IAppointmentService appointmentService;
+        private ICustomerHospitalConsumeService _customerHospitalConsumeService;
+        private ILiveAnchorDailyTargetService _liveAnchorDailyTargetService;
+        public AmiyaAchievementController(IOrderService orderService,
+            IContentPlatformOrderSendService sendContentPlatFormOrderInfoService,
+            IAppointmentService appointmentService,
+            IHttpContextAccessor httpContextAccessor,
+            ICustomerService customerService,
+            IContentPlateFormOrderService contentPlatFormOrderService,
+            IHospitalInfoService hospitalInfoService,
+            IContentPlatFormOrderDealInfoService contentPlatFormOrderDealInfoService,
+            ISendOrderInfoService sendOrderInfoService,
+            ICustomerHospitalConsumeService customerHospitalConsumeService,
+            IShoppingCartRegistrationService shoppingCartRegistrationService,
+            ILiveAnchorDailyTargetService liveAnchorDailyTargetService)
+        {
+            this.orderService = orderService;
+            _sendContentPlatFormOrderInfoService = sendContentPlatFormOrderInfoService;
+            _sendOrderInfoService = sendOrderInfoService;
+            this.httpContextAccessor = httpContextAccessor;
+            this.appointmentService = appointmentService;
+            _contentPlatFormOrderDealInfoService = contentPlatFormOrderDealInfoService;
+            this.customerService = customerService;
+            _hospitalInfoService = hospitalInfoService;
+            _shoppingCartRegistrationService = shoppingCartRegistrationService;
+            _contentPlatFormOrderService = contentPlatFormOrderService;
+            _customerHospitalConsumeService = customerHospitalConsumeService;
+            _liveAnchorDailyTargetService = liveAnchorDailyTargetService;
+        }
+
+        #region 例子，可删
+        /// <summary>
+        /// 获取订单经营情况
+        /// </summary>
+        /// <param name="startDate">开始时间</param>
+        /// <param name="endDate">结束时间</param>
+        /// <returns></returns>
+        [HttpGet("OrderOperationCondition")]
+        [FxInternalAuthorize]
+        public async Task<ResultData<List<OrderOperationConditionVo>>> GetOrderOperationConditionAsync(DateTime? startDate, DateTime? endDate)
+        {
+            TimeSpan timeSpan = endDate.Value - startDate.Value;
+            var date = timeSpan.TotalDays;
+            List<OrderOperationConditionVo> orderOperationCondition = new List<OrderOperationConditionVo>();
+            var q = await orderService.GetOrderOperationConditionAsync(startDate, endDate);
+            for (int x = 0; x <= date; x++)
+            {
+                OrderOperationConditionVo condition = new OrderOperationConditionVo();
+                condition.Date = endDate.Value.AddDays(-x).ToString("yyyy-MM-dd");
+                condition.OrderNum = 0;
+                orderOperationCondition.Add(condition);
+            }
+            foreach (var x in q)
+            {
+                orderOperationCondition.Find(z => z.Date == x.Date).OrderNum = x.OrderNum;
+            }
+            return ResultData<List<OrderOperationConditionVo>>.Success().AddData("orderOperationCondition", orderOperationCondition);
+        }
+        #endregion
+
+        #region 【啊美雅业绩】
+
+        #endregion
+
+        #region 【分组业绩】
+
+        #endregion
+
+        #region 【派单成交业绩】
+
+        #endregion
+    }
+}
