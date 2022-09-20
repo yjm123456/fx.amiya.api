40f65e77997ccfbe1e9afc85dc5c85f7855a1d21
wangjian <1360378350@qq.com>
2022-09-20 20:34:30 +0800

    基础类

---
+++ src/Fx.Amiya.DbModels/BaseDbModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fx.Amiya.DbModels
+{
+    public class BaseDbModel
+    {
+        public string Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public bool Valid { get; set; }
+        public DateTime? DeleteDate { get; set; }
+    }
+}
---
+++ src/Fx.Amiya.Dto/BaseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fx.Amiya.Dto
+{
+    public class BaseDto
+    {
+        public string Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public bool Valid { get; set; }
+        public DateTime? DeleteDate { get; set; }
+    }
+}
