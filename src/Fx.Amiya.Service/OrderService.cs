02ed9e8858a198aad91576b31efb6d0e1bc3fc39
wangjian <1360378350@qq.com>
2022-11-05 17:36:43 +0800

    去除预约医院限制

--- src/Fx.Amiya.Service/OrderService.cs
+++ src/Fx.Amiya.Service/OrderService.cs
@@ -3823,7 +3823,7 @@ namespace Fx.Amiya.Service
                         where d.CustomerId == customerId
                         select d;
             var list = (await order.ToListAsync()).SelectMany(e => e.OrderInfoList);
-            int count = list.Count(e => e.GoodsName.Contains("美肤券") && (e.StatusCode == OrderStatusCode.TRADE_BUYER_PAID||e.StatusCode==OrderStatusCode.TRADE_FINISHED) && !string.IsNullOrEmpty(e.AppointmentHospital));
+            int count = list.Count(e => e.GoodsName.Contains("美肤券") && (e.StatusCode == OrderStatusCode.TRADE_BUYER_PAID||e.StatusCode==OrderStatusCode.TRADE_FINISHED));
             if (count > 0) return true;
             return false;
 
