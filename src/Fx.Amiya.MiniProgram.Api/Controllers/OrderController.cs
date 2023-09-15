10f34b1622cfd2e48633bc186623e8f6583b1048
wangjian <1360378350@qq.com>
2023-09-15 09:02:21 +0800

    支付回调地址替换为线上

--- src/Fx.Amiya.MiniProgram.Api/Controllers/OrderController.cs
+++ src/Fx.Amiya.MiniProgram.Api/Controllers/OrderController.cs
@@ -782,8 +782,8 @@ namespace Fx.Amiya.MiniProgram.Api.Controllers
                     packageInfo.AppId = appId;
                     packageInfo.Body = orderId;
                     //回调地址需重新设置(todo;)                   
-                    //packageInfo.NotifyUrl = string.Format("{0}/amiya/wxmini/Notify/orderpayresult", "https://app.ameiyes.com/amiyamini");
-                    packageInfo.NotifyUrl = string.Format("{0}/amiya/wxmini/Notify/orderpayresult", "http://ymjxui.gnway.cc");
+                    packageInfo.NotifyUrl = string.Format("{0}/amiya/wxmini/Notify/orderpayresult", "https://app.ameiyes.com/amiyamini");
+                    //packageInfo.NotifyUrl = string.Format("{0}/amiya/wxmini/Notify/orderpayresult", "http://ymjxui.gnway.cc");
                     packageInfo.OutTradeNo = tradeId;
                     packageInfo.Attach = tradeId;
                     packageInfo.TotalFee = (int)(totalFee * 100m);
